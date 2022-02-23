16c3e5aa9f15abc2c943111640bb70d7c81bddb3
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-02-22 21:54:55 -0800

    adding exercise for chapter 03_03

--- Exercise-Files/Ch03/03_03/start/react-essentials/src/App.js
+++ Exercise-Files/Ch03/03_03/start/react-essentials/src/App.js
@@ -15,6 +15,11 @@ function Main(props) {
       <p>
         We serve the most {props.adjective} food around.
       </p>
+      <ul style={{ textAlign: "left" }}>
+        {props.dishes.map((dish) => (
+        <li>{dish}</li>
+        ))}
+      </ul>
     </section>
   );
 }
@@ -27,11 +32,18 @@ function Footer(props) {
   );
 }
 
+const dishes = [
+  "Macaroni and Cheese",
+  "Salmon",
+  "Tofu with Vegetables"
+];
+
+
 function App() {
   return (
     <div className="App">
       <Header name="Horacio" />
-      <Main adjective="amazing" />
+      <Main adjective="amazing" dishes={dishes}/>
       <Footer year={new Date().getFullYear()} />
     </div>
   );
