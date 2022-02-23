76e6f879b07ccae54d7aab4137c78483b70a4dfe
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-02-22 19:13:20 -0800

    adding solution for 03_01

--- Exercise-Files/Ch03/03_01/start/react-essentials/src/App.js
+++ Exercise-Files/Ch03/03_01/start/react-essentials/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function Header() {
@@ -9,27 +9,29 @@ function Header() {
     );
   }
 
+function Main() {
+  return(
+    <section>
+      <p>We serve the most delicious food around.</p>
+    </section>
+  );
+}
+
+function Footer() {
+  return(
+    <footer>
+      <p>It's true.</p>
+    </footer>
+  );
+}
+
 function App() {
   return (
-    <div>
-      <Header></Header>
+    <div className='App'>
+      <Header />
+      <Main />
+      <Footer />
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
