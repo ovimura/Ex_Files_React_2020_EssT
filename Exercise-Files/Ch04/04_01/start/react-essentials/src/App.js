b42ada81e5b82371214d25fd2f86732bb0f70c2a
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-03-10 22:14:07 -0800

    adding exercices code

--- Exercise-Files/Ch04/04_01/start/react-essentials/src/App.js
+++ Exercise-Files/Ch04/04_01/start/react-essentials/src/App.js
@@ -9,12 +9,19 @@ function RegularComponent() {
   return <h1>Everyone can see this component.</h1>;
 }
 
+// function App(props) {
+//   return (
+//     <>
+//       {props.authorized ? <SecretComponent /> : <RegularComponent /> }
+//     </>
+//   );
+// }
+
 function App(props) {
-  return (
-    <>
-      {props.authorized ? <SecretComponent /> : <RegularComponent /> }
-    </>
-  );
+  if(props.authorized)
+    return <SecretComponent />;
+  else
+    return <RegularComponent />;
 }
 
 export default App;
