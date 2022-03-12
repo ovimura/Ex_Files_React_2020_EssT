c1c5bca69aaff9b9dfdb4cd1f9565aab0d93f908
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-03-11 17:29:11 -0800

    adding exercices solution

--- Exercise-Files/Ch04/04_03/start/react-essentials/src/App.js
+++ Exercise-Files/Ch04/04_03/start/react-essentials/src/App.js
@@ -1,24 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
-function SecretComponent() {
-  return (
-    <h1>Secret information for authorized users only</h1>
-  );
-}
 
-function RegularComponent() {
-  return <h1>Everyone can see this component.</h1>;
-}
-
-function App({ authorized }) {
+function App() {
+  const [emotion, setEmotion] = useState("happy");
+  console.log(emotion);
   return (
     <>
-      {authorized ? (
-        <SecretComponent />
-      ) : (
-        <RegularComponent />
-      )}
+      <h1>Current emotion is {emotion}.</h1>
+      <button onClick={() => setEmotion("happy")}>Happy</button>
+      <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
+      <button onClick={() => setEmotion("enthusiastic")}>
+        Enthuse
+      </button>
     </>
   );
 }
--- Exercise-Files/Ch04/04_03/start/react-essentials/src/index.js
+++ Exercise-Files/Ch04/04_03/start/react-essentials/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 
-const [, , light] = ["boots", "tent", "headlamp"];
-console.log(light);
+// const [, , light] = ["boots", "tent", "headlamp"];
+// console.log(light);
 
 ReactDOM.render(
   <App authorized={true} />,
