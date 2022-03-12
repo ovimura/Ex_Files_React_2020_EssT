82974f487976ca34baa36ce39391a3fcde644224
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-03-11 18:07:33 -0800

    adding solution exercices

--- Exercise-Files/Ch04/04_05/start/react-essentials/src/App.js
+++ Exercise-Files/Ch04/04_05/start/react-essentials/src/App.js
@@ -1,30 +1,18 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 
 function App() {
-  const [secondary, setSecondary] = useState("tired");
-  useEffect(() => {
-    console.log(`It's ${emotion} around here!`);
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`It's ${secondary} around here.`);
-  }, [secondary]);
+  const [checked, toggle] = useReducer(
+    checked => !checked,
+    false
+    );
 
   return (
     <>
-      <h1>
-        Current emotions are {emotion} and {secondary}.
-      </h1>
-      <button onClick={() => setEmotion("happy")}>
-        Happy
-      </button>
-      <button onClick={() => setEmotion("enthusiastic")}>
-        Enthuse
-      </button>
-      <button onClick={() => setSecondary("crabby")}>
-        Make Crabby
-      </button>
+      <input type="checkbox" value={checked} 
+      onChange={toggle}/>
+
+      <p>{checked ? "checked" : "not checked"}</p>
     </>
   );
 }
