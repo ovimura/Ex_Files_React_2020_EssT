184de0697e1865d26b7368757694d685bb39844d
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-04-03 12:19:00 -0700

    adding exs for CH05_03

--- Exercise-Files/Ch05/05_03/start/react-essentials/src/App.js
+++ Exercise-Files/Ch05/05_03/start/react-essentials/src/App.js
@@ -3,14 +3,24 @@ import "./App.css";
 
 // https://api.github.com/users/eveporcello
 
-function App() {
+function App({login}) {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    if(!login) return;
+    setLoading(true);
     fetch(`https://api.github.com/users/${login}`)
       .then(response => response.json())
-      .then(setData);
-  }, []);
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]);
+
+  if(loading) return <h1>Loading...</h1>;
+  if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if(!data) return null;
 
   if (data) {
     return (
