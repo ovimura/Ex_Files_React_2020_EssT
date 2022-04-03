a4a19dee8fc8d1f662b69291ee656c32b04cd0fd
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-04-03 10:37:54 -0700

    adding exercices for ch 5_1

--- Exercise-Files/Ch05/05_01/start/react-essentials/src/App.js
+++ Exercise-Files/Ch05/05_01/start/react-essentials/src/App.js
@@ -1,20 +1,24 @@
-import React, { useReducer } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
-  const [checked, toggle] = useReducer(
-    checked => !checked,
-    false
-  );
+function App({login}) {
+  
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+    .then(response => response.json())
+    .then(setData);
+  }, []);
+
+  if(data) {
+    return <div>{JSON.stringify(data)}</div>
+  }
+
   return (
-    <>
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={toggle}
-      />
-      <p>{checked ? "checked" : "not checked"}</p>
-    </>
+    <div>
+      No User Available.
+    </div>
   );
 }
 
--- Exercise-Files/Ch05/05_01/start/react-essentials/src/index.js
+++ Exercise-Files/Ch05/05_01/start/react-essentials/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from './App';
 
 const [, , light] = ["boots", "tent", "headlamp"];
 console.log(light);
 
 ReactDOM.render(
-  <App authorized={true} />,
+  <App authorized={true} login="ovimura"/>,
   document.getElementById("root")
 );
