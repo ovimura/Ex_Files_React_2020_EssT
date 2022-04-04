203f28871fa58f77516031487d1b4a67fe29e691
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-04-03 22:05:28 -0700

    adding exercices files updates for Ch 07, 01

--- Exercise-Files/Ch07/07_01/start/react-essentials/src/App.js
+++ Exercise-Files/Ch07/07_01/start/react-essentials/src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home, About, Events, Contact } from "./pages.js";
+
 
 function App() {
-  return <h1>Hello React Testing Library</h1>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- Exercise-Files/Ch07/07_01/start/react-essentials/src/index.js
+++ Exercise-Files/Ch07/07_01/start/react-essentials/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from './App';
+import App from './App.js';
+
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <App login="eveporcello" />,
+  // <App login="eveporcello" />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById("root")
 );
